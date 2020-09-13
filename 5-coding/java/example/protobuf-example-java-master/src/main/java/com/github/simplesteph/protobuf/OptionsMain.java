2b97bb5adf7c26f8de0a284a0209c99412155043
Shubham Jain <shubhamjain2908@gmail.com>
2020-09-13 10:06:30 +0530

    JAVA options

---
+++ 5-coding/java/example/protobuf-example-java-master/src/main/java/com/github/simplesteph/protobuf/OptionsMain.java
@@ -0,0 +1,10 @@
+package com.github.simplesteph.protobuf;
+
+import com.example.options.OptionMessageOther;
+
+public class OptionsMain {
+    public static void main(String[] args) {
+        OptionMessageOther other = OptionMessageOther.newBuilder().build();
+        System.out.println("Other => " + other);
+    }
+}
